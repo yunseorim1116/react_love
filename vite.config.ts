2dc7164956a47fc93af07f4f32ebc3276b9cb784
seorim <hhyjs2r@gmail.com>
2023-05-08 22:15:20 +0900

    [config] 절대경로 설정, setting.json 설정까지 바꿔줘야 import 가능

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tsconfigPaths({
+      // 이 부분에 baseUrl과 paths를 작성합니다.
+      baseUrl: './',
+      paths: {
+        '@/*': ['src/*'],
+        '@components/*': ['src/components/*'],
+      },
+    }),
+  ],
+});
