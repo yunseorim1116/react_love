7d11da2b72d02ab9e482a45d3b3bab3aa68a524b
seorim <hhyjs2r@gmail.com>
2023-05-08 22:17:50 +0900

    [config] tsconfigPaths 라이브러리 vite.config 설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,5 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths({
-      // 이 부분에 baseUrl과 paths를 작성합니다.
-      baseUrl: './',
-      paths: {
-        '@/*': ['src/*'],
-        '@components/*': ['src/components/*'],
-      },
-    }),
-  ],
+  plugins: [react(), tsconfigPaths()],
 });
