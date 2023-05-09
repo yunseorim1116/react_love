b8aa85a1e8f27449885d5827629091c4d47177f2
seorim <hhyjs2r@gmail.com>
2023-05-09 23:31:31 +0900

    [feat] api test 성공

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-
-
+import Title from '@components/common/Title';
+import Button from '@components/common/Button';
+import instance from '@/utils/interceptor';
 const SignIn = () => {
+
+    const testFuction = async ()=>{
+    const res = await instance.get('/test')
+     console.log(res)
+    }
     return (
-        <div>
-        </div>
+        <>
+            <Title title='로그인'/>
+            <div  onClick={testFuction}> api Test </div>
+
+                 <input/>
+                 <br/>
+                 <input/>
+             <br/>
+            <Button buttonText='로그인'/>
+              <br/>
+            <Button buttonText='회원가입 먼저 하기'/>
+        </>
     );
 };
 
