e3bd6ee16f115341b7acc18c2218f294756e4196
seorim <hhyjs2r@gmail.com>
2023-05-10 22:18:52 +0900

    [feat] 회원가입 유효성 검사 로직

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -11,18 +11,69 @@ const SignUp = () => {
     nickname:'',
   })
 
-  const setUserInfo = (e:React.ChangeEvent<HTMLInputElement>)=>{
-    const { name ,value} = e.target
-    serUserInfo({...userInfo, [name] : value})
+  const [formErrors, setFormErrors] = useState({
+  userId: '',
+  password: '',
+  nickname: ''
+});
+
+
+const userIdPattern = /^[a-zA-Z0-9]{3,10}$/;// userId: 3~10 글자, 영어 또는 숫자만 가능
+const nicknamePattern = /^[a-zA-Z0-9가-힣]{3,10}$/;// nickname: 3~10 글자, 영어, 숫자, 한글만 가능
+const passwordPattern = /^.{3,10}$/; // password: 3~10 글자
+
+
+const validateUserId = (userId: string): string => {
+  if (!userIdPattern.test(userId)) {
+    return '아이디는 3~10자의 영문자 또는 숫자로 이루어져야 합니다.';
   }
+  return '';
+};
 
-  const handleSiginIn = async () => {
+const validateNickname = (nickname: string): string => {
+  if (!nicknamePattern.test(nickname)) {
+    return '닉네임은 3~10자의 영문자, 숫자, 한글로 이루어져야 합니다.';
+  }
+  return '';
+};
 
-         try {
-            const res = await instance.post('/auth/signup', userInfo);
-        console.log(res)
-         } catch (e) {
-          console.log(e)
+const validatePassword = (password: string): string => {
+  if (!passwordPattern.test(password)) {
+    return '비밀번호는 3~10자여야 합니다.';
+  }
+  return '';
+};
+
+
+const validateInput = (name: string, value: string) => {
+  switch (name) {
+    case 'userId':
+      return validateUserId(value);
+    case 'nickname':
+      return validateNickname(value);
+    case 'password':
+      return validatePassword(value);
+    default:
+      return '';
+  }
+}
+
+const setUserInfo = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const { name, value } = e.target;
+  serUserInfo({ ...userInfo, [name]: value });
+
+  const errMsg = validateInput(name, value)
+  setFormErrors({ ...formErrors, [name]:errMsg  });
+};
+
+
+
+   const handleSubmit = async () => {
+      try {
+       const res = await instance.post('/auth/signup', userInfo);
+       console.log(res)
+       } catch (e) {
+         console.log(e)
          throw e;
        }
    }
@@ -31,11 +82,14 @@ const SignUp = () => {
         <div>
       <Title title='회원가입'/>
          <input placeholder='아이디' name='userId' onChange={setUserInfo}/>
+         <p>{formErrors.userId}</p>
            < br/>
          <input placeholder='비번' name='password' onChange={setUserInfo}/>
+             <p>{formErrors.password}</p>
           < br/>
          <input placeholder='닉네임' name='nickname' onChange={setUserInfo}/>
-         <Button buttonText='회원가입' handleClickFunction={handleSiginIn}/>
+             <p>{formErrors.nickname}</p>
+         <Button buttonText='회원가입' handleClickFunction={handleSubmit}/>
         </div>
     );
 };
