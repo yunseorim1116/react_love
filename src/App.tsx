635a9f8b821b95de314b4a7e8e3e7313a4bdc94b
seorim <hhyjs2r@gmail.com>
2023-05-10 21:00:10 +0900

    [feat] router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import SignIn from '@/pages/SignIn'
+import SignUp from '@/pages/SignUp';
+import MyPage from '@/pages/MyPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
+import React from 'react';
 
-
+const App = () => {
   return (
-    <>
+    <div>
+      <BrowserRouter>
+         <Routes>
+          <Route path="/" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+             <Route path="/profile" element={<MyPage />} />
+            </Routes>
+       </BrowserRouter>
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
