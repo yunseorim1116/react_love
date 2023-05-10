b286ec5b3c94870242aacb60ede0f3ea0528b9cb
seorim <94429667+yunseorim1116@users.noreply.github.com>
2023-05-10 09:38:18 +0900

    Update index.ts

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -1,1 +1,12 @@
+// axios  new 객체 생성\
 
+import axios form 'axios'
+
+let instance = (baseUrl, timeout ) => new Array()
+
+// interceptor 주입부
+instance = initInterceptor(axios)
+
+// axios 반환 부
+
+return instance 
